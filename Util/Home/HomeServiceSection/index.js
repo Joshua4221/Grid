e38b569488d6e0f86a34e8665d6c71b495cf89d1
collins-okafor <collinszurum@gmail.com>
2022-08-19 17:17:47 +0100

    service cards

--- Util/Home/HomeServiceSection/index.js
+++ Util/Home/HomeServiceSection/index.js
@@ -10,31 +10,31 @@ export const HomeServiceSectionArray = [
   {
     icon: FaLaptopCode,
     header: "Web Development",
-    paragraph: `Holisticly morph distinctive methodologies and vertical e-tailers. Rapidiously grow customized data.`,
+    paragraph: `We build responsive web applications using latest technologies to help businesses of all sizes.`,
   },
   {
     icon: BsFillPhoneFill,
     header: "App Development",
-    paragraph: `Holisticly morph distinctive methodologies and vertical e-tailers. Rapidiously grow customized data.`,
+    paragraph: `Our team of highly experienced developers will help you build mobile applications that drive customer engagement and business growth.`,
   },
   {
     icon: HiShieldCheck,
     header: "Data Security",
-    paragraph: `Holisticly morph distinctive methodologies and vertical e-tailers. Rapidiously grow customized data.`,
+    paragraph: `Our team covers the tools and processes that organizations use to protect information. This includes policy settings that prevent unauthorized people from accessing business or personal information.`,
   },
   {
     icon: FiPenTool,
     header: "UI/UX Design",
-    paragraph: `Holisticly morph distinctive methodologies and vertical e-tailers. Rapidiously grow customized data.`,
+    paragraph: `We have the tools and expertise to help you create an intuitive and engaging user experience that your customers will love.`,
   },
   {
     icon: SiMaterialdesign,
-    header: "Graphics Design",
-    paragraph: `Holisticly morph distinctive methodologies and vertical e-tailers. Rapidiously grow customized data.`,
+    header: "SEO",
+    paragraph: `Get more website traffic, more customers, and more online visibility with powerful and precise SEO services.`,
   },
   {
     icon: TbSpeakerphone,
     header: "Digital Marketing",
-    paragraph: `Holisticly morph distinctive methodologies and vertical e-tailers. Rapidiously grow customized data.`,
+    paragraph: `Need guidance in leveraging the latest technologies? Work with us to benefit from our expertise in Digital Marketing.`,
   },
 ];
